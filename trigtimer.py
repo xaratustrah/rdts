34842a81e2c8411868f290ec4f7f9ae5facd5275
xaratustrah <shahab.sanjari@gmail.com>
2021-02-26 18:27:19 +0100

    .

--- trigtimer.py
+++ trigtimer.py
@@ -60,17 +60,25 @@ def do_trigger():
 
 def start_trigger(trig_time, logfile):
     gpio_setup()
-    print('Triggering every {}. Press ctrl-c to abort.'.format(trig_time))
+    print('Triggering every {}. Press ctrl-c to abort. ctrl-d for immediate trigger'.format(trig_time))
     f = open(logfile, "a")
     try:
         while True:
             current_time = datetime.datetime.now().strftime('%Y-%m-%d@%H:%M:%S.%f')
             print('Triggering at: {}'.format(current_time))
-            do_trigger()
             time.sleep(trig_time)
+            do_trigger()
             f.write(current_time + '\n')
-    except(EOFError, KeyboardInterrupt):
+    except(KeyboardInterrupt):
         print('\nUser input cancelled. Aborting...')
+        f.close()
+    except(EOFError):
+        print('\nManual trigger...')
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d@%H:%M:%S.%f')
+        print('Triggering at: {}'.format(current_time))
+        do_trigger()
+        f.write(current_time + '\n')
+        print('\nNow aborting...')
         f.close()
 
 
